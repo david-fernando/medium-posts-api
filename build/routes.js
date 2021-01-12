81aa94124a545c92ff6b9481ee30e0370653523c
david-fernando <davidfernandodamata21@gmail.com>
2021-01-12 19:28:11 -0300

    updates

--- build/routes.js
+++ build/routes.js
@@ -1,8 +1,13 @@
 "use strict";
 exports.__esModule = true;
 var express_1 = require("express");
+var cors_1 = require("cors");
 var BotController_1 = require("./controllers/BotController");
 var routes = express_1["default"].Router();
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+};
 var botController = BotController_1["default"]();
-routes.get('/', botController.index);
+routes.get('/', cors_1["default"](corsOptions), botController.index);
 exports["default"] = routes;
--- routes.ts
+++ routes.ts
@@ -1,10 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 import BotController from './controllers/BotController'
 
 const routes = express.Router()
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
+
 const botController = BotController()
 
-routes.get('/', botController.index)
+routes.get('/', cors(corsOptions), botController.index)
 
 export default routes

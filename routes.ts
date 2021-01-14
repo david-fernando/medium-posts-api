d814b047b330642949f430e57e050419539ba7a2
david-fernando <davidfernandodamata21@gmail.com>
2021-01-14 11:46:03 -0300

    updates

--- routes.ts
+++ routes.ts
@@ -1,10 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import PostsController from './controllers/PostsController'
 
 const routes = express.Router()
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
 const postsController = PostsController()
 
-routes.get('/', postsController.index)
+routes.get('/', cors(corsOptions), postsController.index)
 
 export default routes

f8bf12595813b0062c3fe4dfcbdd97758b388c34
David Fernando <davidfernandodamata21@gmail.com>
2022-02-22 20:09:41 +0000

    Add automated tests

---
+++ tests/get.test.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+
+describe('Get data from Medium Posts API', ()=>{
+
+    test('Check if it returns 200', async()=>{
+        const dataMedium = await axios.get('https://mediumpostapi.herokuapp.com/?usermedium=davidfernandodamata21') 
+
+        expect(dataMedium.status).toBe(200)
+    })
+} )

98d979e1d6945f50f21ed14203116b48c733030d
David Fernando <davidfernandodamata21@gmail.com>
2022-06-09 21:56:50 +0000

    Add test to data return

--- tests/get.test.ts
+++ tests/get.test.ts
@@ -1,9 +1,47 @@
 import axios from 'axios'
 
 describe('Get data from Medium Posts API', ()=>{
-    test('Check if it returns 200', async()=>{
-        const dataMedium = await axios.get('https://mediumpostapi.herokuapp.com/?usermedium=davidfernandodamata21') 
+    test('Should returns 200', async()=>{
+        const dataMedium = await axios.get(`http://localhost:3333/?usermedium=davidfernandodamata21`) 
         
         expect(dataMedium.status).toBe(200)
+    })
+
+    test('Should returns all data from API', async()=>{
+        const dataMedium = await axios.get(`http://localhost:3333/?usermedium=davidfernandodamata21`) 
+
+        const data = dataMedium.data.dataMedium[0]
+
+        const existTitle = data.hasOwnProperty('title')
+        const existDate = data.hasOwnProperty('date')
+        const existLink = data.hasOwnProperty('link')
+        const existImage = data.hasOwnProperty('image')
+        const existDescription = data.hasOwnProperty('description')
+
+        function existAllData(){
+            if(!existTitle){
+                return;
+            }
+
+            if(!existDate){
+                return;
+            }
+            
+            if(!existLink){
+                return;
+            }
+
+            if(!existImage){
+                return;
+            }
+
+            if(!existDescription){
+                return;
+            }
+
+            return true
+        }
+        
+        expect(existAllData()).toBe(true)
     })
 } )

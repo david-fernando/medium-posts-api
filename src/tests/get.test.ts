de68bd74f951b87be3ac797dba51ed1cf336df5f
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2022-06-22 20:41:00 -0300

    Updated tests with supertest

--- src/tests/get.test.ts
+++ src/tests/get.test.ts
@@ -1,52 +1,35 @@
-import axios from 'axios'
+import supertest from "supertest";
 
-describe('Get data from Medium Posts API', ()=>{
-    test('Should returns 200', async()=>{
-        const dataMedium = await axios.get('http://localhost:3333/?usermedium=davidfernandodamata21')
-        
-        expect(dataMedium.status).toBe(200)
-    })
+import app from "../index";
 
-    test('Should returns all data from API', async()=>{
-        const dataMedium = await axios.get('http://localhost:3333/?usermedium=davidfernandodamata21')
+describe("Get data from Medium Posts API", () => {
+  const request = supertest(app);
+  test("Should returns 200", async () => {
+    const dataMedium = await request.get("/?usermedium=davidfernandodamata21");
 
-        const data = dataMedium.data.dataMedium[0]
+    expect(dataMedium.status).toBe(200);
+  });
 
-        const existTitle = data.hasOwnProperty('title')
-        const existDate = data.hasOwnProperty('date')
-        const existLink = data.hasOwnProperty('link')
-        const existImage = data.hasOwnProperty('image')
-        const existDescription = data.hasOwnProperty('description')
-        const existTags = data.hasOwnProperty('tags')
+  test("should return one post from API", async () => {
+    const dataMedium = await (
+      await request.get("/?usermedium=davidfernandodamata21")
+    ).body;
 
-        function existAllData(){
-            if(!existTitle){
-                return;
-            }
+    const data = dataMedium.dataMedium[0];
 
-            if(!existDate){
-                return;
-            }
-            
-            if(!existLink){
-                return;
-            }
+    expect(data).toEqual(
+      expect.objectContaining({
+        title: expect.any(String),
+        date: expect.any(String),
+        link: expect.any(String),
+        image: expect.any(String),
+        description: expect.any(String),
+        tags: expect.any(Array),
+      })
+    );
+  });
 
-            if(!existImage){
-                return;
-            }
-
-            if(!existDescription){
-                return;
-            }
-
-            if(!existTags){
-                return;
-            }
-
-            return true
-        }
-        
-        expect(existAllData()).toBe(true)
-    })
-} )
+  afterAll(() => {
+    app.close();
+  });
+});

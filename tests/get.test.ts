216d5265235b2e885cf3ea0ab8c9cfa24b877968
David Fernando <davidfernandodamata21@gmail.com>
2022-08-29 15:36:05 +0000

    Change from axios to supertest

--- tests/get.test.ts
+++ tests/get.test.ts
@@ -1,20 +1,24 @@
-import axios from 'axios'
+import supertest from "supertest";
+
+import app from '../pages/api/[usermedium]'
 
 describe("Get data from Medium Posts API", () => {
+    
+    const request = supertest(app);
 
-  const baseUrl = 'https://localhost:3000/api'
+    test("Should returns 200", async () => {
 
-  test("Should returns 200", async () => {
+        const dataMedium = await request.get('/api/?usermedium=davidfernandodamata21');
 
-    const dataMedium = await axios.get(`${baseUrl}/api/davidfernandodamata21`);
-
-    expect(dataMedium.status).toBe(200);
-  });
+        expect(dataMedium.status).toBe(200);
+    });
 
   test("should return one post from API", async () => {
-    const dataMedium = await axios.get(`${baseUrl}/api/usermedium=davidfernandodamata21`)
+    const dataMedium = await (
+        await request.get('/api/davidfernandodamata21')
+    ).body
 
-    const data = dataMedium.data.dataMedium[0];
+    const data = dataMedium.dataMedium[0];
 
     expect(data).toEqual(
       expect.objectContaining({
@@ -29,9 +33,11 @@ describe("Get data from Medium Posts API", () => {
   });
 
   test("Should return placeholder instead image", async() => {
-    const dataMedium = await axios.get(`${baseUrl}/api/getmehiredbootcamp`)
+    const dataMedium = await (
+        await request.get('/api/getmehiredbootcamp')
+    ).body
 
-    const imageUrl = dataMedium.data.dataMedium[1].image;
+    const imageUrl = dataMedium.dataMedium[1].image;
 
     const placeholderUrl = 'https://placehold.jp/bdbdc2/ffffff/250x250.png?text=No%20image'
 

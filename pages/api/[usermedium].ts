3726fe66dc81a809b34b592ce0ff4dfa1e3918d3
David Fernando <davidfernandodamata21@gmail.com>
2022-08-29 14:50:39 +0000

    Add returnJsonPosts

--- pages/api/[usermedium].ts
+++ pages/api/[usermedium].ts
@@ -1,12 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import returnJsonData from '../../utils/returnJsonData'
+import returnJsonPosts from '../../utils/returnJsonPosts'
 
 async function handler( request: NextApiRequest, response: NextApiResponse) {
     const { usermedium } = request.query
 
     const mediumPosts = (usermedium) && await returnJsonData(usermedium.toString())
 
-    response.json({mediumPosts})
+    const dataMedium = returnJsonPosts(mediumPosts, mediumPosts.items)
+
+    response.json({dataMedium})
 }
 
 export default handler

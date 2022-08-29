588e14f99aff7b96dba4887295caf46a05475b13
David Fernando <davidfernandodamata21@gmail.com>
2022-08-29 14:58:04 +0000

    Add try catch

--- pages/api/[usermedium].ts
+++ pages/api/[usermedium].ts
@@ -5,11 +5,15 @@ import returnJsonPosts from '../../utils/returnJsonPosts'
 async function handler( request: NextApiRequest, response: NextApiResponse) {
     const { usermedium } = request.query
 
-    const mediumPosts = (usermedium) && await returnJsonData(usermedium.toString())
+    try{
+        const mediumPosts = (usermedium) && await returnJsonData(usermedium.toString())
 
-    const dataMedium = returnJsonPosts(mediumPosts, mediumPosts.items)
+        const dataMedium = returnJsonPosts(mediumPosts, mediumPosts.items)
 
-    response.json({dataMedium})
+        response.json({dataMedium})
+    }catch{
+        return response.json({ message: 'User not found' })
+    }
 }
 
 export default handler

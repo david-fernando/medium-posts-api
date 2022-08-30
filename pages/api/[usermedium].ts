6e63870ff14fab49efb365b8e037fe2628782821
David Fernando <davidfernandodamata21@gmail.com>
2022-08-30 22:27:30 +0000

    Add NextCors

--- pages/api/[usermedium].ts
+++ pages/api/[usermedium].ts
@@ -1,9 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import NextCors from 'nextjs-cors';
 import returnJsonData from '../../utils/returnJsonData'
 import returnJsonPosts from '../../utils/returnJsonPosts'
 
 async function handler( request: NextApiRequest, response: NextApiResponse) {
     const { usermedium } = request.query
+
+    await NextCors(request, response, {
+        methods: ['GET'],
+        origin: '*',
+        optionsSuccessStatus: 200,
+     });
 
     try{
         const mediumPosts = (usermedium) && await returnJsonData(usermedium.toString())

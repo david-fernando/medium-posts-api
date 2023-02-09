aaabb45c732a59089923e9ed39688b453d116762
David Fernando <davidfernandodamata21@gmail.com>
2023-02-09 21:53:16 +0000

    Add _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Medium Posts API</title>
+        <meta name="Medium Posts API" content="Unofficial Medium API that returns the JSON of your posts" />
+        <link rel="icon" href="/favicon.ico" /> 
+      </Head>       
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp

6e9dcae0d1d1694d075ef2400ea41cb190f8d7f2
David Fernando <davidfernandodamata21@gmail.com>
2023-02-10 15:11:47 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Header from '@/components/Header'
 
+import styles from '@/styles/components/Home.module.css'
+
 function Home(){
     return (
         <div className="container">
             <Header />
+            <div className={styles.banner}></div>
         </div>
     )
 }

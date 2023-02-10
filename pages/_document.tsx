a7ae61633d3b1554cc2dd5538c495f3cbd102c8e
David Fernando <davidfernandodamata21@gmail.com>
2023-02-10 14:58:10 +0000

    Add font Inter from Google Fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap" rel="stylesheet" />
       </Head>
         <body>
           <Main />

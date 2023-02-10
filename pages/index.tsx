adf2aa43fcc162b383481cf6b2cabe4f7c7642be
David Fernando <davidfernandodamata21@gmail.com>
2023-02-10 21:35:54 +0000

    Add banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Header from '@/components/Header'
 
 import styles from '@/styles/components/Home.module.css'
@@ -6,7 +7,19 @@ function Home(){
     return (
         <div className="container">
             <Header />
-            <div className={styles.banner}></div>
+            <div className={styles.banner}>
+                <section>
+                    <div>
+                        <p>Unofficial Medium API that returns the <span>JSON</span> of your posts</p>
+                        <p className={styles.description}>Perfect for developers, bloggers or content creators. Hassle-free access to your Medium articles, in any programming language.</p>
+                        <span>
+                            <input type="text" placeholder='Your Medium username' name="usermedium"/>
+                            <button>Lets go</button>
+                        </span>
+                    </div>
+                </section>
+                <Image src='/banner-background.png' width={651} height={443} alt='Medium Posts API' />
+            </div>
         </div>
     )
 }

8bab9e457bb514c6c3d004b8a90f681ab9ad8d08
David Fernando <davidfernandodamata21@gmail.com>
2023-02-14 22:37:36 +0000

    Add const links

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,39 +8,45 @@ type Props = {
 
 function Header({linkActive = 'home'}: Props){
 
+    const links = {
+        'home': (linkActive === 'home')? styles.active : styles.inactive,
+        'documentation': (linkActive === 'documentation')? styles.active : styles.inactive,
+        'github': (linkActive === 'github')? styles.active : styles.inactive,
+    }
+
     const Links = () => (
         <>
-        <a href="./"
-             className={(linkActive === 'home')? styles.active : styles.inactive}>
+            <a href="./"
+             className={links.home}>
                 Home
             </a>
             <a href="#"
-             className={(linkActive === 'documentation')? styles.active : styles.inactive}>
+             className={links.documentation}>
                 Documentation
             </a>
             <a
              href="https://github.com/david-fernando/medium-posts-api"
              rel="noreferrer" 
              target="_blank"
-             className={(linkActive === 'github')? styles.active : styles.inactive}>
-                Github
+             className={links.github}>
+                GitHub
             </a>
         </>
     )
 
     return (
         <header className={styles.header}>
-        <span>
-            <Image width={64} height={39.8} src="/icon.svg" alt="Medium Posts API" />
-            <p>Medium Posts API</p>
-        </span>
-        <nav>
-            <Links />
-        </nav>
-        <button className={styles.menuButton}>
-            <Image src='menu-icon.svg' width={16} height={17} alt="Menu Button" />
-        </button>
-    </header>
+            <span>
+                <Image width={64} height={39.8} src="/icon.svg" alt="Medium Posts API" />
+                <p>Medium Posts API</p>
+            </span>
+            <nav>
+                <Links />
+            </nav>
+            <button className={styles.menuButton}>
+                <Image src='menu-icon.svg' width={16} height={17} alt="Menu Button" />
+            </button>
+        </header>
     )
 }
 

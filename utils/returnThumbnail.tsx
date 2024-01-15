834cc90ad052891e9915fb727d77763ef7f7d896
David Fernando <davidfernandodamata21@gmail.com>
2024-01-15 15:58:53 +0000

    Fix security problem

--- utils/returnThumbnail.tsx
+++ utils/returnThumbnail.tsx
@@ -1,11 +1,14 @@
+import { load } from "cheerio";
+
 function returnThumbnail(text: string){
     const mediumUrlBlocked = 'https://medium.com/_/stat?event=post'
     const placeholderUrl = 'https://placehold.jp/bdbdc2/ffffff/250x250.png?text=No%20image'
 
-    const regex = /<img\s+[^>]*\bsrc\s*=\s*["']([^"']*)["'][^>]*>/;
-    const match = text.match(regex);
+    // const regex = /<img\s+[^>]*\bsrc\s*=\s*["']([^"']*)["'][^>]*>/;
+    // const match = text.match(regex);
+    const cheerio = load(text)
 
-    let imageUrl = (match && match[1])? match[1] : null;
+    let imageUrl = cheerio('img').first().attr('src')
 
     imageUrl = (imageUrl?.includes(mediumUrlBlocked))? placeholderUrl : imageUrl
 

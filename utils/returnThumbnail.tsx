cea12cde7656f6d70b4201c712c71883105dd341
David Fernando <davidfernandodamata21@gmail.com>
2024-01-15 16:01:38 +0000

    Remove comments

--- utils/returnThumbnail.tsx
+++ utils/returnThumbnail.tsx
@@ -4,8 +4,6 @@ function returnThumbnail(text: string){
     const mediumUrlBlocked = 'https://medium.com/_/stat?event=post'
     const placeholderUrl = 'https://placehold.jp/bdbdc2/ffffff/250x250.png?text=No%20image'
 
-    // const regex = /<img\s+[^>]*\bsrc\s*=\s*["']([^"']*)["'][^>]*>/;
-    // const match = text.match(regex);
     const cheerio = load(text)
 
     let imageUrl = cheerio('img').first().attr('src')

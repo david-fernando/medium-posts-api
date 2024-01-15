a72220b25543d72c42ba8cad4f2862e5c714b993
David Fernando <davidfernandodamata21@gmail.com>
2024-01-15 15:19:57 +0000

    fix image property

--- utils/returnThumbnail.tsx
+++ utils/returnThumbnail.tsx
@@ -1,8 +1,13 @@
-function returnThumbnail(urlImage: string){
-    const mediumUrl = urlImage.split('.clientViewed')[0]
+function returnThumbnail(text: string){
     const mediumUrlBlocked = 'https://medium.com/_/stat?event=post'
     const placeholderUrl = 'https://placehold.jp/bdbdc2/ffffff/250x250.png?text=No%20image'
-    const imageUrl = (mediumUrl === mediumUrlBlocked)? placeholderUrl : urlImage
+
+    const regex = /<img[^>]*src="([^"]*)"[^>]*>/;
+    const match = text.match(regex);
+
+    let imageUrl = (match && match[1])? match[1] : null;
+
+    imageUrl = (imageUrl?.includes(mediumUrlBlocked))? placeholderUrl : imageUrl
 
     return imageUrl
 }
